3b1daf2fe3fa4caa933f015d53c79577ba934ccd
sovnya <enp7s1@tutanota.com>
2025-02-25 19:21:46 +0100

    Implement writing for Lua 5.3 constants
    Im unsure that this works or not, but I believe it does

--- crates/luasleuth-lua53/src/types/constants.rs
+++ crates/luasleuth-lua53/src/types/constants.rs
@@ -1,5 +1,7 @@
 use luasleuth_common::{types::LuaString, CommonCtx};
-use scroll::{ctx, Pread};
+use scroll::{ctx, Pread, Pwrite};
+
+const LUAI_MAXSHORTLEN: usize = 40;
 
 #[derive(Debug)]
 pub enum Constant<'a> {
@@ -8,6 +10,34 @@ pub enum Constant<'a> {
     Float(f64),
     Integer(i64),
     String(LuaString<'a>),
+}
+
+impl Constant<'_> {
+    /// Return the type of the constant in the Lua 5.1 bytecode format.
+    ///
+    /// The values returned are the following:
+    ///
+    /// * `0` for `Nil` constants
+    /// * `1` for `Boolean` constants
+    /// * `3` for `Float` constants
+    /// * `19` for `Integer` constants
+    /// * `4` for short `String` constants
+    /// * `20` for long `String` constants
+    pub fn get_type(&self) -> u8 {
+        match self {
+            Constant::Nil => 0,
+            Constant::Boolean(_) => 1,
+            Constant::Float(_) => 3,
+            Constant::Integer(_) => 19,
+            Constant::String(string) => {
+                if string.size >= LUAI_MAXSHORTLEN {
+                    20
+                } else {
+                    4
+                }
+            }
+        }
+    }
 }
 
 impl<'a> ctx::TryFromCtx<'a, CommonCtx> for Constant<'a> {
@@ -38,6 +68,31 @@ impl<'a> ctx::TryFromCtx<'a, CommonCtx> for Constant<'a> {
     }
 }
 
-// TODO: Implement Lua 5.3 constant writing.
-// NOTE: To implement Lua 5.3 constant writing, we'd have to distinguish between a
-//       long string and a short string
+impl ctx::TryIntoCtx<CommonCtx> for Constant<'_> {
+    type Error = scroll::Error;
+
+    fn try_into_ctx(self, dst: &mut [u8], ctx: CommonCtx) -> Result<usize, Self::Error> {
+        let offset = &mut 0;
+
+        let tag = self.get_type();
+        dst.gwrite_with(tag, offset, ctx.endianness)?;
+
+        match self {
+            Constant::Nil => {}
+            Constant::Boolean(value) => {
+                dst.gwrite_with(value as u8, offset, ctx.endianness)?;
+            }
+            Constant::Float(value) => {
+                dst.gwrite_with(value, offset, ctx.endianness)?;
+            }
+            Constant::Integer(value) => {
+                dst.gwrite_with(value, offset, ctx.endianness)?;
+            }
+            Constant::String(value) => {
+                dst.gwrite_with(value, offset, ctx)?;
+            }
+        };
+
+        Ok(*offset)
+    }
+}
