d485bab5966dcc2b9a94c7df6518812d66c8e395
sovnya <enp7s1@tutanota.com>
2025-02-25 23:23:38 +0100

    Export all lua versions in main luasleuth crate
    They're all under features with the default feature set being all of them enabled

---
+++ crates/luasleuth/src/lib.rs
@@ -0,0 +1,10 @@
+pub use luasleuth_common as common;
+
+#[cfg(feature = "lua51")]
+pub use luasleuth_lua51 as lua51;
+#[cfg(feature = "lua52")]
+pub use luasleuth_lua52 as lua52;
+#[cfg(feature = "lua53")]
+pub use luasleuth_lua53 as lua53;
+#[cfg(feature = "lua54")]
+pub use luasleuth_lua54 as lua54;
