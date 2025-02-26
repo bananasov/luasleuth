2d7ccd9a3a98ca7815219ec496298d66203e3464
sovnya <enp7s1@tutanota.com>
2025-02-26 12:08:51 +0100

    Add lua 5.2 and 5.4 test cases for parsing bytecode

--- crates/luasleuth-lua52/tests/bytecode_binary.rs
+++ crates/luasleuth-lua52/tests/bytecode_binary.rs
@@ -1,24 +1,23 @@
 use luasleuth_common::disassembler::Disassemble as _;
-use luasleuth_lua51::disassembler::Disassembler;
+use luasleuth_lua52::disassembler::Disassembler;
 
 #[test]
 fn test_can_parse_bytecode_file() {
-    let bytes = include_bytes!("../../../data/bytecode/lua51.bin");
+    let bytes = include_bytes!("../../../data/bytecode/lua52.bin");
     let bytecode = Disassembler::new(bytes)
         .disassemble()
         .expect("Failed to read bytecode data");
 
     // Check if most important parts of the disassembly is valid
     assert_eq!(bytecode.header.size_of_size_t, 8);
-    assert_eq!(bytecode.header.version.into_tuple(), (5, 1));
+    assert_eq!(bytecode.header.version.into_tuple(), (5, 2));
 
     // Check prototype related values
     let prototype = bytecode.prototype;
-    assert_eq!(prototype.source, "@.\\example.lua"); // Checks size and actual data\
+    assert_eq!(prototype.debug_info.source, "@.\\example.lua"); // Checks size and actual data
     assert_eq!(prototype.line_defined, 0);
     assert_eq!(prototype.last_line_defined, 0);
-    assert_eq!(prototype.number_of_upvalues, 0);
     assert_eq!(prototype.number_of_parameters, 0);
-    assert_eq!(prototype.is_vararg, 2);
+    assert_eq!(prototype.is_vararg, 1);
     assert_eq!(prototype.max_stack_size, 2);
 }
--- crates/luasleuth-lua54/tests/bytecode_binary.rs
+++ crates/luasleuth-lua54/tests/bytecode_binary.rs
@@ -1,24 +1,28 @@
-use luasleuth_common::disassembler::Disassemble as _;
-use luasleuth_lua51::disassembler::Disassembler;
+use luasleuth_common::{disassembler::Disassemble as _, types::LuaUnsigned};
+use luasleuth_lua54::disassembler::Disassembler;
 
 #[test]
 fn test_can_parse_bytecode_file() {
-    let bytes = include_bytes!("../../../data/bytecode/lua51.bin");
+    let bytes = include_bytes!("../../../data/bytecode/lua54.bin");
     let bytecode = Disassembler::new(bytes)
         .disassemble()
         .expect("Failed to read bytecode data");
 
     // Check if most important parts of the disassembly is valid
-    assert_eq!(bytecode.header.size_of_size_t, 8);
-    assert_eq!(bytecode.header.version.into_tuple(), (5, 1));
+    assert_eq!(
+        bytecode.header.version.into_tuple(),
+        (5, 4),
+        "Got an unexpected Lua version"
+    );
+
+    assert_eq!(bytecode.size_of_upvalues, 1);
 
     // Check prototype related values
     let prototype = bytecode.prototype;
-    assert_eq!(prototype.source, "@.\\example.lua"); // Checks size and actual data\
+    assert_eq!(prototype.source, "@.\\example.lua"); // Checks size and actual data
     assert_eq!(prototype.line_defined, 0);
     assert_eq!(prototype.last_line_defined, 0);
-    assert_eq!(prototype.number_of_upvalues, 0);
     assert_eq!(prototype.number_of_parameters, 0);
-    assert_eq!(prototype.is_vararg, 2);
+    assert_eq!(prototype.is_vararg, 1);
     assert_eq!(prototype.max_stack_size, 2);
 }
