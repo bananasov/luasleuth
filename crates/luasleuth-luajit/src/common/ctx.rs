df8362ebea41e8232c95a36d977c658c49c4526c
bananasov <56086819+bananasov@users.noreply.github.com>
2025-03-23 19:00:40 +0100

    Implement helper functions for LuaJIT Bytecode context

--- crates/luasleuth-luajit/src/common/ctx.rs
+++ crates/luasleuth-luajit/src/common/ctx.rs
@@ -1,6 +1,22 @@
+use scroll::Endian;
+
+/// Flag to determine whether or not bytecode is stripped
+pub const BYTECODE_IS_STRIPPED: u64 = 0x02;
+
 /// A common ctx used between each LuaJIT version
 #[derive(Copy, Clone)]
 pub struct BytecodeContext {
-    /// If the bytecode is stripped
-    pub stripped_bytecode: bool,
+    /// Bytecode flags
+    pub flags: u64,
+
+    /// The endian to read with (when needed)
+    pub endian: Endian,
+}
+
+impl BytecodeContext {
+    /// Determine if the bytecode is stripped of debug information
+    #[inline]
+    pub fn is_stripped(&self) -> bool {
+        self.flags & BYTECODE_IS_STRIPPED == 0
+    }
 }
